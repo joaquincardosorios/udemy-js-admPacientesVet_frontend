f990da5d476daff581c10d1ec2afa1beac0c1d78
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-21 14:55:52 -0400

    feature/login-page

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -4,9 +4,10 @@ import { Outlet } from 'react-router-dom'
 const AuthLayout = () => {
   return (
     <>
-        <h1>Administrador de Pacientes de Veterinaria</h1>
-
-        <Outlet />
+        <main className="container mx-auto md:grid md:grid-cols-2 mt-12 gap-10 p-5 items-center">
+            <Outlet />
+        </main>
+        
     </>
   )
 }
--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,7 +1,59 @@
+import { Link } from 'react-router-dom'
+
 const Login = () => {
   return (
-    <>
-        <h2>Login</h2>
+    <>  
+        <div>
+            <h1 className="text-indigo-600 font-black text-6xl">
+                Inicia Sesión y Administra tus 
+                <span className="text-black"> Pacientes</span>
+            </h1>
+        </div>
+        <div>
+            <form action="">
+                <div className="my-5">
+                    <label 
+                        htmlFor=""
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                    >Email: </label>
+                    <input 
+                        type="email" 
+                        name="" 
+                        id=""
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        placeholder="Email de registro"
+                    />
+                </div>
+                <div>
+                    <label 
+                        htmlFor=""
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                    >Password: </label>
+                    <input 
+                        type="password" 
+                        name="" 
+                        id=""
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        placeholder="Tu Password"
+                    />
+                </div>
+                <input 
+                    type="submit" 
+                    value="Iniciar Sesión" 
+                    className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-8 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                />
+            </form>
+            <nav className='mt-10 lg:flex lg:justify-between'>
+                <Link 
+                    className='block text-center my-5 text-gray-500'
+                    to="/registrar">¿No tienes una cuenta? Registrate
+                </Link>
+                <Link 
+                    className='block text-center my-5 text-gray-500'
+                    to="/olvide-password">Olvide mi Password
+                </Link>
+            </nav>
+        </div>
     </>
   )
 }
