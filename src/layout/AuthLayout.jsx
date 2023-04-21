2011f97eaf56114554bfb6316f42a58d0f350604
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-21 15:26:22 -0400

    feature/register-view

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 const AuthLayout = () => {
   return (
     <>
-        <main className="container mx-auto md:grid md:grid-cols-2 mt-12 gap-10 p-5 items-center">
+        <main className="container mx-auto md:grid md:grid-cols-2 mt-12 gap-5 p-5 md:px-20 items-center">
             <Outlet />
         </main>
         
--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,7 +1,85 @@
+import { Link } from "react-router-dom"
+
 const Registrar = () => {
     return (
       <>
-          <h1>Desde Registrar</h1>
+        <div>
+            <h1 className="text-indigo-600 font-black text-6xl">
+                Crea tu cuenta y Administra 
+                <span className="text-black"> tus Pacientes</span>
+            </h1>
+        </div>
+        <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+          <form action="">
+            <div className="my-5">
+              <label 
+                htmlFor=""
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >Nombre: </label>
+              <input 
+                type="text" 
+                name="" 
+                id=""
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                placeholder="Tu nombre"
+              />
+            </div>
+            <div className="my-5">
+              <label 
+                htmlFor=""
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >Email: </label>
+              <input 
+                type="email" 
+                name="" 
+                id=""
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                placeholder="Tu email"
+              />
+            </div>
+            <div className="my-5">
+              <label 
+                htmlFor=""
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >Password: </label>
+              <input 
+                type="password" 
+                name="" 
+                id=""
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                placeholder="Tu password"
+              />
+            </div>
+            <div className="my-5">
+              <label 
+                htmlFor=""
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >Repetir Password: </label>
+              <input 
+                type="password" 
+                name="" 
+                id=""
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                placeholder="Repite tu password"
+              />
+            </div>
+            <input 
+                    type="submit" 
+                    value="Iniciar Sesión" 
+                    className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-8 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                />
+          </form>
+          <nav className='mt-10 lg:flex lg:justify-between'>
+            <Link 
+              className='block text-center my-5 text-gray-500'
+              to="/">Inicia Sesion
+            </Link>
+            <Link 
+              className='block text-center my-5 text-gray-500'
+              to="/olvide-password">Olvide mi Password
+            </Link>
+            </nav>
+        </div>
       </>
     )
   }
