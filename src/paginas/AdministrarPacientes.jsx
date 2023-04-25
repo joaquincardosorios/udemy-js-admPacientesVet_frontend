ce5767b8598aa8b349f4b5df8cf67ca7be883193
Joaquin <joaquin.rios@crecic.cl>
2023-04-25 14:19:39 -0400

    feature/patient-form-responsive

--- src/paginas/AdministrarPacientes.jsx
+++ src/paginas/AdministrarPacientes.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react"
 import Formulario from "../components/Formulario"
 import ListadoPacientes from "../components/ListadoPacientes"
 
 const AdministrarPacientes = () => {
+
+  const [mostrarFormulario, setMostrarFormulario] = useState(false)
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="md:w-1/2 lg:w-2/5">
+      <button
+        type="button"
+        className="bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-m mb-5 md:hidden"
+        onClick={() => setMostrarFormulario(!mostrarFormulario) }
+      >{mostrarFormulario ? "Ocultar Formulario" : "Mostrar Formulario"}</button>
+      <div className={`${mostrarFormulario ? 'block' : 'hidden'} md:block md:w-1/2 lg:w-2/5`}>
         <Formulario />
       </div>
       <div className="md:w-1/2 lg:w-3/5">
