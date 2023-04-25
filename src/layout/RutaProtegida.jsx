ad9d7f8ef99b130e34586fa4ae4f4832be37f9f2
Joaquin <joaquin.rios@crecic.cl>
2023-04-25 10:24:57 -0400

    feature/footer

--- src/layout/RutaProtegida.jsx
+++ src/layout/RutaProtegida.jsx
@@ -1,4 +1,6 @@
 import { Outlet, Navigate } from "react-router-dom"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 import useAuth from "../hooks/useAuth"
 
 const RutaProtegida = () => {
@@ -10,9 +12,9 @@ const RutaProtegida = () => {
     if (cargando) return 'cargando...'
     return (
         <>
-            <h1>Esta es una ruta protegida</h1>
-
-            { auth.perfil?._id ? <Outlet /> : <Navigate to="/" />}
+            <Header/>
+                { auth.perfil?._id ? <Outlet /> : <Navigate to="/" />}
+            <Footer />
         </>
         
     )
